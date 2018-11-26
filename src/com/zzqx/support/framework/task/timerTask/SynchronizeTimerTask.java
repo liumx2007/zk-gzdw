ce6ac998b074a5c274e447a8b13b9a5249e664ff
wjw0215 <821060818@qq.com>
2018-11-26 09:59:28 +0800

    Update SynchronizeTimerTask.java

--- src/com/zzqx/support/framework/task/timerTask/SynchronizeTimerTask.java
+++ src/com/zzqx/support/framework/task/timerTask/SynchronizeTimerTask.java
@@ -62,50 +62,51 @@ public class SynchronizeTimerTask {
     /**
      * 每天获取排版和人员信息同步
      */
+    @Transactional
     public void doSynchronizeTask(){
-//        BhSchdu bhSchdu = new BhSchdu();
-//        EmployeeInformation employeeInformation = new EmployeeInformation();
-//        getData = HttpUtil.get(CountInfo.GET_SYNC_DAYA);
-        String getData = "{\n" +
-                "\t\"ScheduList\": [],\n" +
-                "\t\"employeeInformationList\": [{\n" +
-                "\t\t\"averageProcessTime\": \"24min\",\n" +
-                "\t\t\"bindState\": \"1\",\n" +
-                "\t\t\"businessToday\": \"200\",\n" +
-                "\t\t\"createBy\": \"1\",\n" +
-                "\t\t\"createTime\": \"2018-07-17\",\n" +
-                "\t\t\"delFlag\": \"0\",\n" +
-                "\t\t\"employeeAccount\": \"a005\",\n" +
-                "\t\t\"employeeAge\": \"27\",\n" +
-                "\t\t\"employeeId\": \"SF0008\",\n" +
-                "\t\t\"employeeSex\": \"1\",\n" +
-                "\t\t\"employeeState\": \"1\",\n" +
-                "\t\t\"hallId\": 1,\n" +
-                "\t\t\"id\": 2,\n" +
-                "\t\t\"jobs\": \"5,2\",\n" +
-                "\t\t\"loginName\": \"3333\",\n" +
-                "\t\t\"myWork\": \"岗位名称\",\n" +
-                "\t\t\"name\": \"张颖彤\",\n" +
-                "\t\t\"password\": \"333\",\n" +
-                "\t\t\"phone\": \"1325556565\",\n" +
-                "\t\t\"updateBy\": \"1\",\n" +
-                "\t\t\"updateTime\": \"2018-08-21\",\n" +
-                "\t\t\"workState\": \"1\"\n" +
-                "\t}]\n" +
-                "}";
+//        String getData = "{\n" +
+//                "\t\"ScheduList\": [],\n" +
+//                "\t\"employeeInformationList\": [{\n" +
+//                "\t\t\"averageProcessTime\": \"24min\",\n" +
+//                "\t\t\"bindState\": \"1\",\n" +
+//                "\t\t\"businessToday\": \"200\",\n" +
+//                "\t\t\"createBy\": \"1\",\n" +
+//                "\t\t\"createTime\": \"2018-07-17\",\n" +
+//                "\t\t\"delFlag\": \"0\",\n" +
+//                "\t\t\"employeeAccount\": \"a005\",\n" +
+//                "\t\t\"employeeAge\": \"27\",\n" +
+//                "\t\t\"employeeId\": \"SF0008\",\n" +
+//                "\t\t\"employeeSex\": \"1\",\n" +
+//                "\t\t\"employeeState\": \"1\",\n" +
+//                "\t\t\"hallId\": 1,\n" +
+//                "\t\t\"id\": 2,\n" +
+//                "\t\t\"jobs\": \"5,2\",\n" +
+//                "\t\t\"loginName\": \"3333\",\n" +
+//                "\t\t\"myWork\": \"岗位名称\",\n" +
+//                "\t\t\"name\": \"张颖彤\",\n" +
+//                "\t\t\"password\": \"333\",\n" +
+//                "\t\t\"phone\": \"1325556565\",\n" +
+//                "\t\t\"updateBy\": \"1\",\n" +
+//                "\t\t\"updateTime\": \"2018-08-21\",\n" +
+//                "\t\t\"workState\": \"1\"\n" +
+//                "\t}]\n" +
+//                "}";
+        getData = HttpUtil.get(CountInfo.GET_SYNC_DAYA);
         JSONObject dataJson = JSONUtil.parseObj(getData);
         Object bhSchduObject = dataJson.get("ScheduList");
         Object employeeInformationObject = dataJson.get("employeeInformationList");
-        JSONArray schJsonArray = JSONUtil.parseArray(dataJson.get("ScheduList"));
-        JSONArray empJsonArray = JSONUtil.parseArray(dataJson.get("employeeInformationList"));
+        JSONArray schJsonArray = JSONUtil.parseArray(bhSchduObject);
+        JSONArray empJsonArray = JSONUtil.parseArray(employeeInformationObject);
 
         List<BhSchdu> schduList = JSONUtil.toList(schJsonArray,BhSchdu.class );
         List<EmployeeInformation> employeeInformationList = JSONUtil.toList(empJsonArray,EmployeeInformation.class );
 
         if(schduList !=null && schduList.size()>0){
+            bhSchduMapper.deleteByExample(null);
             bhSchduMapper.batchInsertBhSchdu(schduList);
         }
         if(employeeInformationList != null && employeeInformationList.size()>0){
+            employeeInformationMapper.deleteByExample(null);
             employeeInformationMapper.batchInsert(employeeInformationList);
         }
   
