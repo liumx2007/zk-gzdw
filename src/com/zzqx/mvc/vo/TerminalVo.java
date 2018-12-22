6a6ebff84c1e8454383721ca71c7ed7fb2f2afdf
wjw0215 <821060818@qq.com>
2018-12-22 11:57:06 +0800

    terminal  List

---
+++ src/com/zzqx/mvc/vo/TerminalVo.java
@@ -0,0 +1,70 @@
+package com.zzqx.mvc.vo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class TerminalVo {
+    private String id;
+    private int serialNumber;
+    private String status;
+    private String name;
+    private String codeName;
+    private String ip;
+    private String mac;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+}
